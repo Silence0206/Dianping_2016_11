26abd90d5f07ab1459e0c745f35d723e38dfdfd7
Silence0206 <732379020@qq.com>
2017-01-15 18:12:49 +0800

    add

---
+++ Feature_extract/create feature.py
@@ -0,0 +1,75 @@
+# *- coding: utf-8 -*-
+from gensim import corpora, models, similarities
+from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder
+
+import  text_processing as tp
+import os
+
+
+
+# rews = ["这家店经过好几次，一贯的印象是生意不错再加上非常大，非常干净整洁，能让食客看到自己食物制作的整个过程。",
+#         "地道的本帮菜，挺有特点。价格刚才一查才知道他名字后面加“精作坊”。环境不错，菜的口味还是很适应不同的人群，菜的质量可以，有不少包房，很适应一般商务用餐。",
+#         "冲着面包蛤蜊汤去的～幸好是没到饭点就去抢位子,价格还行～海鲜面么无功无过～就是一股西餐的味道啦～～～"]
+
+#从数据库读评论并调用函数进行数据清洗
+results = tp.read_list_for_BusArea('root', '58424716', 'dianping','r812')
+comments = []
+
+for item in results:
+    comments.append(item[11])
+print("===========打印评论=========")
+count = 1
+for com in comments:
+    print(str(count)+"    "+com)
+    count+=1
+
+commentList = tp.seg_fil_rew(comments)
+def tarin1(texts):
+    dictionary = corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    tfidf = models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=10)
+    # lda = models.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=2)
+    print("=====打印LSI10个主题=====")
+    lsiout=lsi.print_topics(10)
+    for item in lsiout:
+            print(item)
+    print("=====输出多个文档的文档内容和各主题的匹配性大小关系=====")
+    corpus_lsi = lsi[corpus_tfidf]
+    for doc in corpus_lsi:  #######输出多个文档的文档内容和各主题的匹配性大小关系
+            print(doc)  #######输出为该文档的内容和各个主题的匹配性大小关系
+    index = similarities.MatrixSimilarity(lsi[corpus])
+    return (index, dictionary, lsi)
+
+
+(index,dictionary,lsi) =  tarin1(commentList)
+#要处理的对象登场
+target_courses = ['环境还行，但是感觉不是很好吃，排队的人太多了']
+target_text = tp.seg_fil_rew(target_courses)
+"""
+对具体对象相似度匹配
+"""
+
+# 选择一个基准数据
+ml_course = target_text[0]
+
+# 词袋处理
+ml_bow = dictionary.doc2bow(ml_course)
+print("================词袋处理后的一句话================")
+print(ml_bow)#把该查询文档（词集）更改为（词袋模型）即：字典格式，key是单词，value是该单词在该文档中出现次数。
+
+# 在上面选择的模型数据 lsi 中，计算其他数据与其的相似度
+ml_lsi = lsi[ml_bow]  # ml_lsi 形式如 (topic_id, topic_value)
+print("================ml_lsi================")
+print(ml_lsi)
+sims = index[ml_lsi]  # sims 是最终结果了， index[xxx] 调用内置方法 __getitem__() 来计算ml_lsi
+
+# 排序，为输出方便
+sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+# 查看结果
+print(sort_sims[0:10] ) # 看下前10个最相似的，第一个是基准数据自身
+
+
+
---
+++ test.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding=gbk -*-
+from gensim import corpora, models, similarities
+from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder
+
+
+"""
+     原始数据，用于建立模型
+"""
+# 缩水版的courses，实际数据的格式应该为 课程名\t课程简介\t课程详情，并已去除html等干扰因素
+courses = [
+    u'Writing II: Rhetorical Composing',
+    u'Genetics and Society: A Course for Educators',
+    u'General Game Playing',
+    u'Genes and the Human Condition (From Behavior to Biotechnology)',
+    u'A Brief History of Humankind',
+    u'New Models of Business in Society',
+    u'Analyse Numrique pour Ingnieurs',
+    u'Evolution: A Course for Educators',
+    u'Coding the Matrix: Linear Algebra through Computer Science Applications',
+    u'The Dynamic Earth: A Course for Educators',
+    u'Tiny Wings\tYou have always dreamed of flying - but your wings are tiny. Luckily the world is full of beautiful hills. Use the hills as jumps - slide down, flap your wings and fly! At least for a moment - until this annoying gravity brings you back down to earth. But the next hill is waiting for you already. Watch out for the night and fly as fast as you can. ',
+    u'Angry Birds Free',
+    u'没有\它很相似',
+    u'没有\t它很相似',
+    u'没有\t他很相似',
+    u'没有\t他不很相似',
+    u'没有',
+    u'可以没有',
+    u'也没有',
+    u'有没有也不管',
+    u'Angry Birds Stella',
+    u'Flappy Wings - FREE\tFly into freedom!A parody of the #1 smash hit game!',
+    u'没有一个',
+    u'没有一个2',
+]
+
+# 只是为了最后的查看方便
+# 实际的 courses_name = [course.split('\t')[0] for course in courses]
+courses_name = courses
+path="G:\Dianping2017\Dianping_2016_11"
+"""
+    预处理(easy_install nltk)
+"""
+
+
+def pre_process_cn(courses, low_freq_filter=True):
+    """
+     简化的 中文+英文 预处理
+        1.去掉停用词
+        2.去掉标点符号
+        3.处理为词干
+        4.去掉低频词
+
+    """
+    import nltk
+    import jieba.analyse
+    from nltk.tokenize import word_tokenize
+
+    texts_tokenized = []
+    for document in courses:
+        texts_tokenized_tmp = []
+        for word in word_tokenize(document):
+            texts_tokenized_tmp += jieba.analyse.extract_tags(word, 10)
+        texts_tokenized.append(texts_tokenized_tmp)
+
+    texts_filtered_stopwords = texts_tokenized
+
+    # 去除标点符号
+    english_punctuations = [',', '.', ':', ';', '?', '(', ')', '[', ']', '&', '!', '*', '@', '#', '$', '%']
+    texts_filtered = [[word for word in document if not word in english_punctuations] for document in
+                      texts_filtered_stopwords]
+
+    # 词干化
+    from nltk.stem.lancaster import LancasterStemmer
+    st = LancasterStemmer()
+    texts_stemmed = [[st.stem(word) for word in docment] for docment in texts_filtered]
+
+    # 去除过低频词
+    if low_freq_filter:
+        all_stems = sum(texts_stemmed, [])
+        stems_once = set(stem for stem in set(all_stems) if all_stems.count(stem) == 1)
+        texts = [[stem for stem in text if stem not in stems_once] for text in texts_stemmed]
+    else:
+        texts = texts_stemmed
+    return texts
+
+lib_texts = pre_process_cn(courses)
+print(lib_texts)
+"""
+    引入gensim，正式开始处理(easy_install gensim)
+"""
+
+#http://www.52nlp.cn/%E5%A6%82%E4%BD%95%E8%AE%A1%E7%AE%97%E4%B8%A4%E4%B8%AA%E6%96%87%E6%A1%A3%E7%9A%84%E7%9B%B8%E4%BC%BC%E5%BA%A6%E4%BA%8C
+def train_by_lsi(lib_texts):
+    """
+        通过LSI模型的训练
+    """
+    from gensim import corpora, models, similarities
+
+    # 为了能看到过程日志
+    # import logging
+    # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    dictionary = corpora.Dictionary(lib_texts)
+    dictionary.save(path+'/tmp/deerwester.dict')
+    print("==token2id=")
+    print(dictionary.token2id)
+    #{'angry': 13, 'socy': 0, '相似': 16, 'cours': 2, 'gen': 3, 'bird': 15, 'ear': 5, 'hil': 10, 'gam': 4, 'down': 12, 'fre': 14, 'wing': 6, 'fly': 7, '一个': 18, 'tiny': 11, 'but': 8, 'yo': 9, 'educ': 1, '没有': 17}
+    corpus = [dictionary.doc2bow(text) for text in
+              lib_texts]  # doc2bow(): 将collection words 转为词袋，用两元组(word_id, word_frequency)表示
+    print(corpus)
+    tfidf = models.TfidfModel(corpus)
+    print(tfidf)
+    corpus_tfidf = tfidf[corpus] #基于这个TF-IDF模型，我们可以将上述用词频表示文档向量表示为一个用tf-idf值表示的文档向量 也就是原来的词频变成TF-IDF值
+    # for doc in corpus_tfidf: #http://www.cnblogs.com/hanacode/articles/4819328.html
+    #     print(doc)
+
+    # 拍脑袋的：训练topic数量为10的LSI模型
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=11)
+    for t in lsi.print_topics(11):
+        print(t)#一行一个topic
+    index = similarities.MatrixSimilarity(lsi[corpus])  # index 是 gensim.similarities.docsim.MatrixSimilarity 实例
+
+    return (index, dictionary, lsi)
+
+# train_by_lsi(lib_texts)
+dic = corpora.Dictionary.load(path+'/tmp/deerwester.dict')
+print(dic)
