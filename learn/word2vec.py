e7476a6730d48efa10136974fc5b860d4b528dd6
Silence0206 <732379020@qq.com>
2017-02-11 12:32:25 +0800

    add

--- learn/word2vec.py
+++ learn/word2vec.py
@@ -95,6 +95,8 @@ if __name__ == '__main__':
     getMostSimilar("面料","word2vec_Fc_filter_sent.model",100)
     getMostSimilar("大小","word2vec_Fc_filter_sent.model",100)
     getMostSimilar("快递","word2vec_Fc_filter_sent.model",100)
+    getMostSimilar("服务","word2vec_Fc_filter_sent.model",100)
+
 
 
 
